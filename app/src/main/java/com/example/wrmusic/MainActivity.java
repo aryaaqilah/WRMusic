d5a45e081bd89f6acc3dba57f756cbe8ba2afbdc
aryaaqilah <111030604+aryaaqilah@users.noreply.github.com>
2024-08-14 21:37:24 +0700

    item_detail

--- app/src/main/java/com/example/wrmusic/MainActivity.java
+++ app/src/main/java/com/example/wrmusic/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.wrmusic;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
     }
+
 }

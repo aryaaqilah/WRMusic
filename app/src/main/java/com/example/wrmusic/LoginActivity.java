f8567a237b2f7ffa59fb0788c9f44ab140f75357
robanpam <arikusumadewi3@gmail.com>
2024-08-14 20:43:10 +0700

    login fe

---
+++ app/src/main/java/com/example/wrmusic/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.wrmusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.fonts.Font;
+import android.os.Bundle;
+
+import java.io.File;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
+
+
